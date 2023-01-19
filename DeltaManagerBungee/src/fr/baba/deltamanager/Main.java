6090db30571dd33e6aeca52c11154f4cb14da35d
Baba1408 <baba1408.mail@gmail.com>
2023-01-20 00:45:24 +0100

    Better explanations at /reload (when certain modules are disabled)

--- DeltaManagerBungee/src/fr/baba/deltamanager/Main.java
+++ DeltaManagerBungee/src/fr/baba/deltamanager/Main.java
@@ -3,6 +3,7 @@ package fr.baba.deltamanager;
 import java.util.ArrayList;
 
 import fr.baba.deltamanager.commands.DeltaManager;
+import fr.baba.deltamanager.events.PlayerChat;
 import fr.baba.deltamanager.events.PlayerDisconnect;
 import fr.baba.deltamanager.events.PlayerLogin;
 import fr.baba.deltamanager.events.PostLogin;
@@ -11,12 +12,14 @@ import fr.baba.deltamanager.events.ServerSwitch;
 import fr.baba.deltamanager.managers.MonitorManager;
 import fr.baba.deltamanager.managers.UpdatesManager;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginManager;
 
 public class Main extends Plugin {
 	private static Main instance;
-	static String prefix = "[BungeeManager]";
+	static String prefix = "[DeltaBungeeManager]";
+	static Boolean isStarting = true;
 	
 	static ArrayList<String> events = new ArrayList<>();
 	
@@ -39,22 +42,22 @@ public class Main extends Plugin {
 		pm.registerCommand(this, new DeltaManager("dm"));
 		
 		load();
+		UpdatesManager.check();
+		isStarting = false;
 	}
 	
 	public static Main getInstance(){
 		return instance;
 	}
 	
-	@SuppressWarnings("deprecation")
 	public static void reload(){
 		CommandSender cs = getInstance().getProxy().getConsole();
-		cs.sendMessage(prefix + " Reload config...");
+		cs.sendMessage(TextComponent.fromLegacyText(prefix + " Reload config..."));
 		Config.setup();
 		load();
-		cs.sendMessage(prefix + " The configuration has been successfully reloaded!");
+		cs.sendMessage(TextComponent.fromLegacyText(prefix + " The configuration has been successfully reloaded!"));
 	}
 	
-	@SuppressWarnings("deprecation")
 	public static void load(){
 		PluginManager pm = instance.getProxy().getPluginManager();
 		CommandSender cs = instance.getProxy().getConsole();
@@ -63,26 +66,26 @@ public class Main extends Plugin {
 		if(Config.getConfig().getBoolean("save-connexion.enabled") && !events.contains("ServerKick")){
 			pm.registerListener(instance, new ServerKick());
 			events.add("ServerKick");
-			cs.sendMessage("ServerKick event started");
-		}
+			cs.sendMessage(TextComponent.fromLegacyText("ServerKick event started"));
+		} else if(!isStarting && !Config.getConfig().getBoolean("save-connexion.enabled") && events.contains("ServerKick")) cs.sendMessage(TextComponent.fromLegacyText("§cTo completely disable the \"Save-connexion\" module you will need to restart the server"));
 		
 		if((Config.getConfig().getBoolean("logs.login.enabled") || Config.getConfig().getBoolean("webhook.login.enabled")) && !events.contains("PlayerLogin")){
 			pm.registerListener(instance, new PlayerLogin());
 			events.add("PlayerLogin");
-			cs.sendMessage("PlayerLogin event started");
+			cs.sendMessage(TextComponent.fromLegacyText("PlayerLogin event started"));
 		}
 		
 		if((Config.getConfig().getBoolean("logs.switch.enabled") || Config.getConfig().getBoolean("webhook.switch.enabled")) && !events.contains("ServerSwitch")){
 			pm.registerListener(instance, new ServerSwitch());
 			events.add("ServerSwitch");
-			cs.sendMessage("ServerSwitch event started");
+			cs.sendMessage(TextComponent.fromLegacyText("ServerSwitch event started"));
 		}
 		
 		if((Config.getConfig().getBoolean("logs.disconnect.enabled") || Config.getConfig().getBoolean("webhook.disconnect.enabled")
 				|| Config.getConfig().getBoolean("logs.nomoreplayers.enabled") || Config.getConfig().getBoolean("logs.nomoreplayers.enabled")) && !events.contains("PlayerDisconnect")){
 			pm.registerListener(instance, new PlayerDisconnect());
 			events.add("PlayerDisconnect");
-			cs.sendMessage("PlayerDisconnect event started");
+			cs.sendMessage(TextComponent.fromLegacyText("PlayerDisconnect event started"));
 		}
 		
 		if(Config.getConfig().getBoolean("monitor.enabled")){
@@ -91,11 +94,22 @@ public class Main extends Plugin {
 			if(Config.getConfig().getBoolean("monitor.notify.on-join.enabled") && !events.contains("PostLogin")){
 				pm.registerListener(instance, new PostLogin());
 				events.add("PostLogin");
-				cs.sendMessage("PlayerDisconnect event started");
+				cs.sendMessage(TextComponent.fromLegacyText("PlayerDisconnect event started"));
 			}
-		}
+		} else if(!isStarting && !Config.getConfig().getBoolean("monitor.enabled") &&events.contains("PostLogin")) cs.sendMessage(TextComponent.fromLegacyText("§cTo completely disable the \"Monitor\" module you will need to restart the server"));
 		
-		UpdatesManager.check();
+		if(Config.getConfig().getBoolean("chat.enabled")){
+			PlayerChat.init();
+			
+			if(!events.contains("PlayerChat")){
+				pm.registerListener(instance, new PlayerChat());
+				events.add("PlayerChat");
+				cs.sendMessage(TextComponent.fromLegacyText("PlayerChat event started"));
+			}
+		} else if(!isStarting && events.contains("PlayerChat")){
+			PlayerChat.clear();
+			cs.sendMessage(TextComponent.fromLegacyText("§cTo completely disable the \"Chat\" module you will need to restart the server"));
+		}
 	}
 	
 	@Override
