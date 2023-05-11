87ed390cff3f65467aba2b27b0e9873f3fb453aa
Baba1408 <baba1408.mail@gmail.com>
2023-05-11 21:59:02 +0200

    Remove unnecessary code

--- DeltaManagerBungee/src/fr/baba/deltamanager/Main.java
+++ DeltaManagerBungee/src/fr/baba/deltamanager/Main.java
@@ -6,12 +6,6 @@ import java.util.Map.Entry;
 import fr.baba.deltamanager.commands.BungeeTeleport;
 import fr.baba.deltamanager.commands.DeltaManager;
 import fr.baba.deltamanager.commands.ServerQueue;
-import fr.baba.deltamanager.events.PlayerChat;
-import fr.baba.deltamanager.events.PlayerDisconnect;
-import fr.baba.deltamanager.events.PlayerLogin;
-import fr.baba.deltamanager.events.PostLogin;
-import fr.baba.deltamanager.events.ServerKick;
-import fr.baba.deltamanager.events.ServerSwitch;
 import fr.baba.deltamanager.managers.ModulesManager;
 import fr.baba.deltamanager.managers.MonitorManager;
 import fr.baba.deltamanager.managers.UpdatesManager;
@@ -84,57 +78,6 @@ public class Main extends Plugin {
 		} else cs.sendMessage(TextComponent.fromLegacyText("§2[DeltaManagerBungee]§a Reloading..."));
 		
 		ModulesManager.reload();
-		
-		//Events
-		if(false){ //old code soon it will be removed
-		if(Config.getConfig().getBoolean("save-connexion.enabled") && !events.contains("ServerKick")){
-			pm.registerListener(instance, new ServerKick());
-			events.add("ServerKick");
-			cs.sendMessage(TextComponent.fromLegacyText("ServerKick event started"));
-		} else if(!isStarting && !Config.getConfig().getBoolean("save-connexion.enabled") && events.contains("ServerKick")) cs.sendMessage(TextComponent.fromLegacyText("§cTo completely disable the \"Save-connexion\" module you will need to restart the server"));
-		
-		if((Config.getConfig().getBoolean("logs.login.enabled") || Config.getConfig().getBoolean("webhook.login.enabled")) && !events.contains("PlayerLogin")){
-			pm.registerListener(instance, new PlayerLogin());
-			events.add("PlayerLogin");
-			cs.sendMessage(TextComponent.fromLegacyText("PlayerLogin event started"));
-		}
-		
-		if((Config.getConfig().getBoolean("logs.switch.enabled") || Config.getConfig().getBoolean("webhook.switch.enabled")) && !events.contains("ServerSwitch")){
-			pm.registerListener(instance, new ServerSwitch());
-			events.add("ServerSwitch");
-			cs.sendMessage(TextComponent.fromLegacyText("ServerSwitch event started"));
-		}
-		
-		if((Config.getConfig().getBoolean("logs.disconnect.enabled") || Config.getConfig().getBoolean("webhook.disconnect.enabled")
-				|| Config.getConfig().getBoolean("logs.nomoreplayers.enabled") || Config.getConfig().getBoolean("logs.nomoreplayers.enabled")) && !events.contains("PlayerDisconnect")){
-			pm.registerListener(instance, new PlayerDisconnect());
-			events.add("PlayerDisconnect");
-			cs.sendMessage(TextComponent.fromLegacyText("PlayerDisconnect event started"));
-		}
-		
-		if(Config.getConfig().getBoolean("monitor.enabled")){
-			MonitorManager.init();
-			
-			if(Config.getConfig().getBoolean("monitor.notify.on-join.enabled") && !events.contains("PostLogin")){
-				pm.registerListener(instance, new PostLogin());
-				events.add("PostLogin");
-				cs.sendMessage(TextComponent.fromLegacyText("PlayerDisconnect event started"));
-			}
-		} else if(!Config.getConfig().getBoolean("monitor.enabled") && events.contains("PostLogin")) cs.sendMessage(TextComponent.fromLegacyText("§cTo completely disable the \"Monitor\" module you will need to restart the server"));
-		
-		if(Config.getConfig().getBoolean("chat.enabled")){
-			PlayerChat.init();
-			
-			if(!events.contains("PlayerChat")){
-				pm.registerListener(instance, new PlayerChat());
-				events.add("PlayerChat");
-				cs.sendMessage(TextComponent.fromLegacyText("PlayerChat event started"));
-			}
-		} else if(events.contains("PlayerChat")){
-			PlayerChat.clear();
-			cs.sendMessage(TextComponent.fromLegacyText("§cTo completely disable the \"Chat\" module you will need to restart the server"));
-		}
-		}
 	}
 	
 	@Override
